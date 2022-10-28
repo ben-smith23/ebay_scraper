f01d9b93a807fb02018c16f687145934caf06f34
ben-smith23 <112433581+ben-smith23@users.noreply.github.com>
2022-10-28 10:32:56 -0700

    Update ebay-dl.py

--- ebay-dl.py
+++ ebay-dl.py
@@ -1,10 +1,25 @@
-from bs4 import BeautifulSoup as soup
-
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--text1')
-args = parser.parse_args()
-text4 = args.text1
-from urllib.request import urlopen as Ureq
-from datetime import datetime
 import requests
+from bs4 import BeautifulSoup as soup
+import bs4
+
+# get command line arguments
+parser = argparse.ArgumentParser(description='download ebay information and convert to JSON')
+parser.add_argument('search_term')
+args = parser.parse_args()
+
+print('args.search_terms=', args.search_term)
+
+for page_number in range(1,11):
+    
+    # build the url
+    url = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=' + args.search_term + '&_sacat=0&_pgn=' + str(page_number)
+    print('url=', url)
+
+    #download the html
+    r = requests.get(url)
+    status = r.status_code
+    print('status=', status)
+
+    html = r.text
+    #print('html=', html[:50])
